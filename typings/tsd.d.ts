e556b3a32160fbb2be8345ce72fd24684c3e14f4
MirekSz <miro1994>
2016-02-29 19:42:19 +0100

    tests in app

--- typings/tsd.d.ts
+++ typings/tsd.d.ts
@@ -3,3 +3,7 @@
 /// <reference path="react/react-dom.d.ts" />
 /// <reference path="react/react.d.ts" />
 /// <reference path="redux/redux.d.ts" />
+/// <reference path="chai/chai.d.ts" />
+/// <reference path="jquery/jquery.d.ts" />
+/// <reference path="mocha/mocha.d.ts" />
+/// <reference path="superagent/superagent.d.ts" />
