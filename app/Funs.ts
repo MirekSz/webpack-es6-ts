a013b19465aa6242d5cb726447303833b2066f0f
Mirek <m.szajo>
2016-03-09 14:00:49 +0100

    jsx

--- app/Funs.ts
+++ app/Funs.ts
@@ -11,9 +11,11 @@ export function start() {
 }
 class CustomerDto {
     name:string;
+    @younger
+    prev:number;
     age:number;
 
-    constructor(name:string,age:number) {
+    constructor(name:string, age:number) {
         this.name = name;
         this.age = age;
 
@@ -25,11 +27,39 @@ interface CustomerService<T> {
 }
 
 class CustomerServiceImpl implements CustomerService<CustomerDto> {
+    @log
     sayHello(name:string):Promise<any> {
-        return new Promise<any>( (resolve, reject)=> {
-            setTimeout(() =>{
+        return new Promise<any>((resolve, reject)=> {
+            setTimeout(() => {
                 resolve(new CustomerDto(name, 30));
             }, 1000);
         });
     }
 }
+
+new CustomerServiceImpl().sayHello('mirek').then(function (data) {
+    console.log('data: ');
+    console.log(data);
+    console.log(data.prev);
+});
+
+function log(target:Object, propertyKey:string, descriptor:TypedPropertyDescriptor<any>) {
+    var originalMethod = descriptor.value; // save a reference to the original method
+
+    // NOTE: Do not use arrow syntax here. Use a function expression in
+    // order to use the correct value of `this` in this method (see notes below)
+    descriptor.value = function (...args:any[]) {
+        console.log("The method args are: " + JSON.stringify(args)); // pre
+        var result = originalMethod.apply(this, args);               // run and store the result
+        console.log("The return value is: " + result);               // post
+        return result;                                               // return the result of the original method
+    };
+
+    return descriptor;
+}
+
+function younger(target:Object, key:string) {
+    target[key] = 15
+}
+
+//https://gist.github.com/remojansen/16c661a7afd68e22ac6e
